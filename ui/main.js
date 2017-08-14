ea43fa91f6d045ecdc28916950c16b7610cd9d6c
IndulekhaP <indulekhap2810715@gmail.com>
2017-08-15 02:15:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,6 @@ var element = document.getElementById('main-text');
 element.innerHTML = "New value";
 
 var img = document.getElementById('madi');
-    alert(img);
 img.onclick = function(){
     alert("dd");
     img.style.marginLeft = '100px';   
