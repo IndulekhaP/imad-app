9f7502e3d7eb60d38e82bd9bbdf06893bd1a3f6c
IndulekhaP <indulekhap2810715@gmail.com>
2017-08-15 00:18:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -70,6 +70,14 @@ app.get('/article-one', function (req, res) {
 });
 
 app.get('/article-two', function (req, res) {
+  res.send(createTemplate(articles.articleTwo));
+});
+
+app.get('/article-three', function (req, res) {
+  res.send(createTemplate(articles.articleThree));
+});
+
+app.get('/article-two', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
